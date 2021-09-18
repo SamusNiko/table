85fab3c46b7c0b2828bc1351a08535e723ea1783
Egor-Kozlov <kozlov1998.2010@gmail.com>
2021-09-18 21:36:57 +0300

    feat: deleted cocsole.log and fix footer

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -7,9 +7,6 @@ const Table = () => {
     const [inputSearch, setinputSearch] = useState('') //* <- here TwoSideBinding for search input 
     const [countPages, setcountPages] = useState([]) //* <- count pages in pagination
     const [currentPage, setcurrentPage] = useState(1) //* <- current selected page
-
-    const [number, setnumber] = useState()
-
     const [sortingField, setsortingField] = useState('')
     const [sortingOrder, setsortingOrder] = useState('asc')
 
@@ -17,11 +14,11 @@ const Table = () => {
 
     const headers = [
         { name: 'ID', field: 'id', sortable: true},
-        { name: 'First name', field: 'firstName', sortable: true},
-        { name: 'Last name', field: 'lastName', sortable: true},
+        { name: 'First name', field: 'first-name', sortable: true},
+        { name: 'Last name', field: 'lastname', sortable: true},
         { name: 'Email', field: 'email', sortable: true},
         { name: 'Phone', field: 'phone', sortable: true},
-        { name: 'State', field: 'state', sortable: true}
+        { name: 'State', field: 'state', sortable: false}
     ]
 
     useEffect(() => {
@@ -36,7 +33,6 @@ const Table = () => {
     }, [])
 
     useEffect(() => {
-        console.log(123);
         const countPage = () => {
             const arr = []
             for (let index = 1; index <= peopleList.length / ITEMS_PER_PAGE; index++) {
@@ -48,16 +44,7 @@ const Table = () => {
         visibleList()   
     }, [peopleList, inputSearch])
 
-    useEffect(() => {
-        visibleList()
-    }, [currentPage])
-
-    useEffect(() => {
-        onInputSearch()
-    }, [inputSearch])
-
     const onInputSearch = () => {
-        console.log(inputSearch);
         let arr = []
         if (inputSearch) {
             arr = peopleList.filter((e) => 
@@ -121,10 +108,8 @@ const Table = () => {
         const indexOfLastItem = currentPage * ITEMS_PER_PAGE //* <- we find the last element
         const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE //* <- we find the first element
 
-        
         if (!inputSearch) {
             const resultList = (list.slice(indexOfFirstItem , indexOfLastItem))
-            console.log('length',resultList.length);
             return resultList    
         } else {
             let resultList = list.filter((e) => 
@@ -134,8 +119,6 @@ const Table = () => {
                 e.phone.includes(inputSearch)
                 )
             resultList = resultList.slice(indexOfFirstItem , indexOfLastItem)
-            console.log('length',resultList.length);
-
             return resultList
         }
     }
@@ -153,7 +136,6 @@ const Table = () => {
     }
 
     return (
-        console.log('render'),
         <div className="table-container">
             <h1 className='tible-title'>ITRex test project</h1>
             <div className='table'>
@@ -161,19 +143,14 @@ const Table = () => {
                     <input className='table__input-search' type="text" value = {inputSearch}
                         onChange = {(event) => setinputSearch(event.target.value)}
                         placeholder='Search Users by Name, Email, Phone' />
-                    <select>
-                        <option>Пункт 1</option>
-                        <option>Пункт 2</option>
-                    </select>
                 </div>
                     <ul className="table__body">
                         <li className="table__row table__row--default">
                             <ul className='person__list'>
-
                                 {headers.map(({ name, field, sortable }) => {
                                     return (
                                         <li className={`person__item person__item--${field}`} 
-                                                onClick = {() => sortable ?  onSortingChange(field) : null} 
+                                                onClick = {() => sortable ? onSortingChange(field) : null} 
                                                 key = {name}>
                                             {name}
                                             {sortingField && sortingField === field ? (sortingOrder === "asc"
@@ -182,15 +159,8 @@ const Table = () => {
                                         </li>
                                     )
                                 })}
-                                {/* <li className='person__item person__item--id'>ID</li>
-                                <li className='person__item person__item--first-name' onClick = {() => sort()}>First name</li>
-                                <li className='person__item person__item--lastname'>Last name</li>
-                                <li className='person__item person__item--email'>Email</li>
-                                <li className='person__item person__item--phone'>Phone</li>
-                                <li className='person__item person__item--state'>State</li> */}
                             </ul>
                         </li>
-                        {/*Error here about Each child in a list should have a unique "key"*/}
                         {visibleList().map((person) => {
                             return (
                                 <li onClick = {() => visibleListPerson(person)} className="table__row" key = {person.number}>
@@ -208,7 +178,6 @@ const Table = () => {
                     </ul>
                 <div className="table__footer">
                     <div className="pagination">
-                        {/* fix when <1 pages */}
                         <ul className = 'pagination__list'>
                             <li className = 'pagination__item'  onClick = {() => onMinusPage()}>Previous</li>
                                 {countPages.length > 1 ? 
